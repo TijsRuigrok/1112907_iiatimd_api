9d2c88396bf22778e59cd37f3ab2269c57813164
Tijs Ruigrok <TijsRuigrok15@gmail.com>
2021-08-22 20:08:37 +0200

    removed unnecessary seeders

--- database/seeds/ChoreSeeder.php
+++ database/seeds/ChoreSeeder.php
@@ -12,14 +12,5 @@ class ChoreSeeder extends Seeder
     public function run()
     {
         
-        DB::table('chores')->insert([
-            'guid' => 'fe407b76-697e-44e3-8186-07aae7b64475',
-            'name' => 'Cook dinner',
-            'points' => '200',
-            'date' => '14 July 2021',
-            'completed' => 0,
-            'user_id' => 1,
-        ]);
-        
     }
 }
